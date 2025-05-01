e5d72b472fcc295c8778c12beece580924f54c78
Sasha <aleksagerhardt@gmail.com>
2025-05-01 12:21:05 +0700

    Добавлен проект для тестирования.

---
+++ lr3Test/lr3Test.cpp
@@ -0,0 +1,16 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace lr3Test
+{
+	TEST_CLASS(lr3Test)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
